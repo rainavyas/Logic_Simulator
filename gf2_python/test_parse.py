5f84973b9bed583abf0fe8bdd43303b898819e82
James Crossley <jkc34@cam.ac.uk>
2019-05-28 17:54:43 +0100

    First parser tests working and 'test_def_file.txt' errors corrected.

--- gf2_python/test_parse.py
+++ gf2_python/test_parse.py
@@ -9,32 +9,42 @@ from network import Network
 
 @pytest.fixture
 def trial_names():
-    "Create a names instance."
     return Names()
 
 @pytest.fixture
-def trial_devices():
-    "Create a devices instance"
-    return devices(names_mod)
+def trial_devices(trial_names):
+    """Create a devices instance"""
+    return Devices(trial_names)
 
 @pytest.fixture
-def trial_network():
-    "Create network instance"
-    return network(trial_names, trial_devices)
+def trial_network(trial_names, trial_devices):
+    """Create network instance"""
+    return Network(trial_names, trial_devices)
 
 @pytest.fixture
-def trial_monitors():
-    "Creates monitors instance"
-    return monitors(trial_names, trial_devices, trial_network)
+def trial_monitors(trial_names, trial_devices, trial_network):
+    """Creates monitors instance"""
+    return Monitors(trial_names, trial_devices, trial_network)
 
 @pytest.fixture
-def trial_scanner1():
-    "Create scanner for first file."
-    path1  = "parse_test_files/test_1.txt"
-    return scanner(path1, trial_names)
+def trial_scanner1(trial_names):
+    path = 'parse_test_files/test_1(incorrect).txt'
+    scanner1 = Scanner(path, trial_names)
+    return scanner1
 
-def test_parse_network():
+@pytest.fixture
+def trial_scanner2(trial_names):
+    path = 'parse_test_files/test_2(correct).txt'
+    scanner2 = Scanner(path, trial_names)
+    return scanner2
+
+def test_parse_network(trial_names, trial_devices, trial_network,
+                    trial_monitors, trial_scanner1, trial_scanner2):
     "Feed a file known to be incorrect."
-    test_parse = Parser(trial_names, trial_devices, trial_network,
+
+    test_parse1 = Parser(trial_names, trial_devices, trial_network,
                         trial_monitors, trial_scanner1)
-    assert test_parse.parse_network == False
+    test_parse2 = Parser(trial_names, trial_devices, trial_network,
+                        trial_monitors, trial_scanner2)
+    assert test_parse1.parse_network() == False
+    assert test_parse2.parse_network() == True

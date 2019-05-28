0635a54d728fafa16f2d1e6fa3557de9e144738e
rainavyas <sayvaniar@gmail.com>
2019-05-28 11:10:14 +0100

    Created errors class to store all errors

---
+++ gf2_python/errors.py
@@ -0,0 +1,19 @@
+"""Record the error messages and location.
+
+Used in the Logic Simulator project to record the specified error
+
+Classes
+-------
+Error - records the particular errors
+
+"""
+
+class Error:
+    "DOCSTRING stuff"
+
+    def __init__(self, msg, line_num, line, carat_pos):
+
+        self.msg = msg
+        self.line_num = line_num
+        self.line = line
+        self.carat_pos = carat_pos
